0f2cebd8bcf38b1ffec45c181e850ed601763e00
Bilal Azam <bilal.azam1@outlook.com>
2023-04-04 00:00:00 +0100

    Add user authentication with JWT and initial user model

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const port = 5000;
@@ -7,6 +8,9 @@ const port = 5000;
 mongoose.connect('mongodb://localhost:27017/smart-task-manager', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
---
+++ backend/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+  res.status(201).send('User registered');
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !await bcrypt.compare(password, user.password)) {
+    return res.status(400).send('Invalid credentials');
+  }
+  const token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '1h' });
+  res.json({ token });
+});
+
+module.exports = router;
