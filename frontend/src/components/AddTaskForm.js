fad7e754856b3dad526fc7f66bd13e4b6a0ca4f6
Bilal Azam <bilal.azam1@outlook.com>
2023-04-10 00:00:00 +0100

    Create AddTaskForm component for adding new tasks

---
+++ frontend/src/components/AddTaskForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTaskAsync } from '../store/taskSlice';
+
+const AddTaskForm = () => {
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTaskAsync({ title }));
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter task title"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default AddTaskForm;
