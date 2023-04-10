4ed62c9b51992c3ac9461211b4642d924613a1ee
Bilal Azam <bilal.azam1@outlook.com>
2023-04-11 00:00:00 +0100

    Integrate AddTaskForm into TaskList and adjust styling

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchTasks } from '../store/taskSlice';
 import TaskItem from './TaskItem';
+import AddTaskForm from './AddTaskForm';
 
 const TaskList = () => {
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ const TaskList = () => {
   return (
     <div>
       <h2>Task List</h2>
+      <AddTaskForm />
       <ul>
         {tasks.map((task) => (
           <TaskItem key={task._id} task={task} />
