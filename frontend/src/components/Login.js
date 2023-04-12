56a4fc5e7c767599697b202ff8d69fe0686889ad
Bilal Azam <bilal.azam1@outlook.com>
2023-04-13 00:00:00 +0100

    Create Login component with authentication integration

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { login } from '../utils/auth';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/auth/login', { username, password });
+      login(response.data.token);
+      history.push('/');
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
