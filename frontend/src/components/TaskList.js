898a2b82678a7cead7c4199de7a268d3cb9d72ba
Bilal Azam <bilal.azam1@outlook.com>
2023-04-14 00:00:00 +0100

    Implement logout functionality and update UI

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -3,18 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchTasks } from '../store/taskSlice';
 import TaskItem from './TaskItem';
 import AddTaskForm from './AddTaskForm';
+import { logout } from '../utils/auth';
+import { useHistory } from 'react-router-dom';
 
 const TaskList = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const tasks = useSelector((state) => state.tasks.tasks);
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
 
+  const handleLogout = () => {
+    logout();
+    history.push('/login');
+  };
+
   return (
     <div>
       <h2>Task List</h2>
+      <button onClick={handleLogout}>Logout</button>
       <AddTaskForm />
       <ul>
         {tasks.map((task) => (
