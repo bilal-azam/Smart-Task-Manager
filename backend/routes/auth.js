b74c03324ae250c5394fbc34209a02b04203267c
Bilal Azam <bilal.azam1@outlook.com>
2023-04-27 00:00:00 +0100

    Add user registration functionality and validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,15 +4,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Login route
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+// Registration route
+router.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: 'User already exists' });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
 
     const payload = {
       user: {
@@ -29,16 +37,6 @@ router.post('/login', async (req, res) => {
         res.json({ token });
       }
     );
-  } catch (err) {
-    res.status(500).json({ msg: 'Server error' });
-  }
-});
-
-// Get user profile route
-router.get('/profile', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
   } catch (err) {
     res.status(500).json({ msg: 'Server error' });
   }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,43 +1,50 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const history = useHistory();
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    if (username === '' || password === '') {
-      setError('Both fields are required');
-      return;
-    }
     try {
-      await axios.post('http://localhost:5000/auth/register', { username, password });
-      history.push('/login');
-    } catch (error) {
-      setError('Registration failed');
+      const response = await axios.post('http://localhost:5000/auth/register', { username, email, password });
+      localStorage.setItem('token', response.data.token);
+      navigate('/');
+    } catch (err) {
+      setMessage('Registration failed. User might already exist.');
     }
   };
 
   return (
     <div>
       <h2>Register</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
+      {message && <p>{message}</p>}
+      <form onSubmit={handleRegister}>
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
+          required
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
         />
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
+          required
         />
         <button type="submit">Register</button>
       </form>
