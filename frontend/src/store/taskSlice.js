14f154d31373e30feb7dcc0f0af48b4a67ef148c
Bilal Azam <bilal.azam1@outlook.com>
2023-05-02 00:00:00 +0100

    Refactor Redux store for better task state management

--- frontend/src/store/taskSlice.js
+++ frontend/src/store/taskSlice.js
@@ -1,23 +1,23 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import * as taskApi from '../api/taskApi';
+import axios from 'axios';
 
 export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
-  const response = await taskApi.getTasks();
+  const response = await axios.get('http://localhost:5000/tasks');
   return response.data;
 });
 
-export const addTask = createAsyncThunk('tasks/addTask', async (task) => {
-  const response = await taskApi.createTask(task);
+export const addTask = createAsyncThunk('tasks/addTask', async (newTask) => {
+  const response = await axios.post('http://localhost:5000/tasks', newTask);
   return response.data;
 });
 
-export const updateTask = createAsyncThunk('tasks/updateTask', async ({ id, task }) => {
-  const response = await taskApi.updateTask(id, task);
+export const updateTask = createAsyncThunk('tasks/updateTask', async ({ id, updatedTask }) => {
+  const response = await axios.put(`http://localhost:5000/tasks/${id}`, updatedTask);
   return response.data;
 });
 
 export const deleteTask = createAsyncThunk('tasks/deleteTask', async (id) => {
-  await taskApi.deleteTask(id);
+  await axios.delete(`http://localhost:5000/tasks/${id}`);
   return id;
 });
 
@@ -25,26 +25,36 @@ const taskSlice = createSlice({
   name: 'tasks',
   initialState: {
     tasks: [],
-    status: null,
+    status: 'idle',
+    error: null
   },
+  reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = 'loading';
+      })
       .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = 'succeeded';
         state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
       })
       .addCase(addTask.fulfilled, (state, action) => {
         state.tasks.push(action.payload);
       })
       .addCase(updateTask.fulfilled, (state, action) => {
         const index = state.tasks.findIndex((task) => task._id === action.payload._id);
-        if (index !== -1) {
+        if (index >= 0) {
           state.tasks[index] = action.payload;
         }
       })
       .addCase(deleteTask.fulfilled, (state, action) => {
         state.tasks = state.tasks.filter((task) => task._id !== action.payload);
       });
-  },
+  }
 });
 
 export default taskSlice.reducer;
